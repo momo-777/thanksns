1169144f7951624627482826a275464babad3017
鶴岡もも <momo1999729@gmail.com>
2021-05-30 22:12:54 +0900

    first-commit

--- app/javascript/new.js
+++
@@ -1,6 +0,0 @@
-const colorpicker = document.querySelector('.arrow-icon-color-selector');
-  const arrowicon = document.getElementById('arrow-icon');
-
-  colorpicker.addEventListener('input', function(e) {
-    document.body.style.color = e.target.value;
-  });
